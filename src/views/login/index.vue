22fa961398578b0c24a4c5e23b3912c4c59ab49e
xtt5221 <1395408068@qq.com>
2020-05-10 00:32:39 +0800

    完成登录注册的基本功能

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,20 +1,102 @@
 <template>
-  <div class="login-container">登录页面</div>
+  <div class="login-container">
+    <van-nav-bar
+      class="app-nav-bar"
+      title="注册 / 登录"
+      left-arrow
+      @click-left="$router.back()"
+    />
+    <!-- 登录表单 -->
+    <van-cell-group>
+      <van-field
+        v-model="user.mobile"
+        icon-prefix="toutiao"
+        left-icon="shouji"
+        placeholder="请输入手机号"
+      />
+      <van-field
+        v-model="user.code"
+        icon-prefix="toutiao"
+        left-icon="yanzhengma"
+        clearable
+        placeholder="请输入验证码"
+      >
+        <template #button>
+          <van-button
+          class="send-btn"
+          size="small"
+          round
+          >发送验证码</van-button>
+        </template>
+      </van-field>
+    </van-cell-group>
+    <!-- 登录按钮 -->
+    <div class="login-btn-wrap">
+      <van-button
+      class="login-btn"
+      type="info"
+      block
+      @click="onLogin()"
+      >登录</van-button>
+    </div>
+  </div>
 </template>
+
 <script>
+import { login } from '@/api/user'
+
 export default {
   name: 'LoginIndex',
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    async onLogin () {
+      // 请求调用登录
+      try {
+        const res = await login(this.user)
+        // 处理响应结果
+        console.log(res)
+      } catch (err) {
+        console.log(err)
+        console.log('登录失败', err)
+      }
+    }
+  }
 }
 </script>
+
 <style scoped lang='less'>
+.login-container {
+  .send-btn {
+    width: 76px;
+    height: 23px;
+    background-color: #ededed;
+    .van-button__text {
+    font-size: 11px;
+    color: #666666;
+    }
+  }
+}
+.login-btn-wrap {
+  padding: 26px 16px;
+  .login-btn {
+    background-color: #6db4fb;
+    border: none;
+    .van-button__text {
+      font-size: 15px;
+    }
+  }
+}
 </style>
