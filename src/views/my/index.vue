4b6d102c535c8c05c39cf749be8fc59cc0d4ccff
xtt5221 <1395408068@qq.com>
2020-05-12 12:42:22 +0800

    完成我的页面的基本样式

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,50 +1,52 @@
 <template>
   <div class="my-container">
     <!-- 头部 -->
-    <van-cell-group>
+    <van-cell-group class="my-info">
       <van-cell
+      class="base-info"
         title="单元格"
         value="内容"
         center
+        :border="false"
         >
         <van-image
+          class="avatar"
           slot="icon"
-          width="50"
-          height="50"
           fit="cover"
           round
           src="https://img.yzcdn.cn/vant/cat.jpeg"
         />
-        <div slot="title">昵称</div>
+        <div class="name" slot="title">昵称</div>
         <van-button
+          class="update-btn"
           size="small"
           round
         >编辑资料</van-button>
       </van-cell>
 
       <!-- 头条情况 -->
-      <van-grid>
-        <van-grid-item>
-          <div slot="text">
-            <div class="span">123</div>
+      <van-grid class="data-info" :border="false">
+        <van-grid-item class="data-info-item">
+          <div slot="text" class="text-wrap">
+            <div class="count">123</div>
             <div class="text">头条</div>
           </div>
         </van-grid-item>
-        <van-grid-item>
-          <div slot="text">
-            <div class="span">123</div>
+        <van-grid-item class="data-info-item">
+          <div slot="text" class="text-wrap">
+            <div class="count">123</div>
             <div class="text">关注</div>
           </div>
         </van-grid-item>
-        <van-grid-item>
-          <div slot="text">
-            <div class="span">123</div>
+        <van-grid-item class="data-info-item">
+          <div slot="text" class="text-wrap">
+            <div class="count">123</div>
             <div class="text">粉丝</div>
           </div>
         </van-grid-item>
-        <van-grid-item>
-          <div slot="text">
-            <div class="span">123</div>
+        <van-grid-item class="data-info-item">
+          <div slot="text" class="text-wrap">
+            <div class="count">123</div>
             <div class="text">获赞</div>
           </div>
         </van-grid-item>
@@ -52,13 +54,15 @@
     </van-cell-group>
 
     <!-- 收藏和历史 -->
-    <van-grid :column-num="2">
+    <van-grid class="nav-grid mb-4" :column-num="2">
       <van-grid-item
+        class="nav-grid-item"
         icon-prefix="toutiao"
         icon="shoucang"
         text="收藏"
       />
       <van-grid-item
+        class="nav-grid-item"
         icon-prefix="toutiao"
         icon="lishi"
         text="历史"
@@ -67,10 +71,11 @@
 
     <!-- 小智和退出登录 -->
     <van-cell title="消息通知" is-link to="/" />
-    <van-cell title="小智同学" is-link to="/" />
-    <van-cell title="退出登录" />
+    <van-cell class="mb-4" title="小智同学" is-link to="/" />
+    <van-cell class="logout-cell" title="退出登录" />
   </div>
 </template>
+
 <script>
 export default {
   name: 'MyIndex',
@@ -86,5 +91,81 @@ export default {
   methods: {}
 }
 </script>
+
 <style scoped lang='less'>
+.my-container {
+  .my-info {
+    background: url(./banner.png) no-repeat;
+    background-size: cover;
+    .base-info {
+      box-sizing: border-box;
+      height: 115px;
+      padding-top: 38px;
+      padding-bottom: 11px;
+      background-color: unset;
+      .avatar {
+        box-sizing: border-box;
+        width: 66px;
+        height: 66px;
+        border: 1px solid #fff;
+        margin-right: 11px;
+      }
+      .name {
+        font-size: 15px;
+        color: #fff;
+      }
+      .update-btn {
+        height: 16px;
+        font-size: 10px;
+        color: #666;
+      }
+    }
+    .data-info {
+      .data-info-item {
+        height: 65px;
+        color: #fff;
+        .text-wrap {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          .count {
+            font-size: 18px;
+          }
+          .text {
+            font-size: 11px;
+          }
+        }
+      }
+    }
+    /deep/ .van-grid-item__content {
+      background-color: unset;
+    }
+  }
+  /deep/ .nav-grid {
+    .nav-grid-item {
+      height: 70px;
+      .toutiao {
+        font-size: 22px;
+      }
+      .toutiao-shoucang {
+        color: #eb5253;
+      }
+      .toutiao-lishi {
+        color: #ff9d1d;
+      }
+      .van-grid-item__text {
+        font-size: 14px;
+        color: #333;
+      }
+    }
+  }
+  .logout-cell {
+    text-align: center;
+    color: #d86262;
+  }
+  .mb-4 {
+    margin-bottom: 4px;
+  }
+}
 </style>
