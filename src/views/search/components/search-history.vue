b53fca7e24621368f3eefba9c063b6baf343344a
xtt5221 <1395408068@qq.com>
2020-05-18 19:16:12 +0800

    删除所有历史记录

--- src/views/search/components/search-history.vue
+++ src/views/search/components/search-history.vue
@@ -1,21 +1,33 @@
 <template>
   <div class="search-history">
     <van-cell title="搜索历史">
-      <div>
-        <span>全部删除</span>
+      <div v-if="isDeleteShow">
+        <span
+          @click="$emit('update-histories', [])"
+        >全部删除</span>
         &nbsp;&nbsp;
-        <span>删除</span>
+        <span @click="isDeleteShow = false">完成</span>
       </div>
+      <van-icon
+        v-else
+        name="delete"
+        @click="isDeleteShow = true"
+      />
     </van-cell>
     <van-cell
       :title="history"
       v-for="(history, index) in searchHistories"
       :key="index"
+      @click="onDeleteShow(history, index)"
     >
-       <van-icon name="close" />
+       <van-icon
+        name="close"
+        v-show="isDeleteShow"
+      />
      </van-cell>
   </div>
 </template>
+
 <script>
 export default {
   name: 'SearchHistory',
@@ -27,14 +39,28 @@ export default {
     }
   },
   data () {
-    return {}
+    return {
+      isDeleteShow: false // 删除显示状态
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    onDeleteShow (history, index) {
+      // 删除状态.执行删除操作
+      if (this.isDeleteShow) {
+        this.searchHistories.splice(index, 1)
+
+        return
+      }
+      // 非删除状态,展示搜索结果
+      this.$emit('search', history)
+    }
+  }
 }
 </script>
+
 <style scoped lang='less'>
 </style>
--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -27,7 +27,9 @@
     <!-- 历史记录 -->
     <search-history
       v-else
-      :search-histories="searchHistroies"
+      :search-histories="searchHistories"
+      @search="onSearch"
+      @update-histories="searchHistories = $event"
     />
   </div>
 </template>
@@ -36,7 +38,7 @@ import SearchSuggestion from './components/search-suggestion'
 import SearchHistory from './components/search-history'
 import SearchResult from './components/search-result'
 import { setItem, getItem } from '@/utils/storage'
-import { getSearchHistories } from '@/api/search'
+// import { getSearchHistories } from '@/api/search'
 import { mapState } from 'vuex'
 
 export default {
@@ -51,13 +53,18 @@ export default {
     return {
       searchText: '', // 搜索输入框的内容
       isResultShow: false, // 控制搜索结果的显示状态
-      searchHistroies: getItem('search-histories') || [] // 搜索历史记录
+      searchHistories: getItem('search-histories') || [] // 搜索历史记录
     }
   },
   computed: {
     ...mapState(['user'])
   },
-  watch: {},
+  watch: {
+    // 监视搜索历史记录的变化,存储到本地
+    searchHistories () {
+      setItem('search-histories', this.searchHistories)
+    }
+  },
   created () {
     this.loadSearchHiestories()
   },
@@ -67,17 +74,14 @@ export default {
       // 把输入框设置为需要搜索的文本
       this.searchText = searchText
 
-      const index = this.searchHistroies.indexOf(searchText)
+      const index = this.searchHistories.indexOf(searchText)
       if (index !== -1) {
         // 把重复项删除
-        this.searchHistroies.splice(index, 1)
+        this.searchHistories.splice(index, 1)
       }
 
       // 把最新的搜索记录放到顶部
-      this.searchHistroies.unshift(searchText)
-
-      // 如果没有登录,则把搜索记录保存到本地
-      setItem('search-histories', this.searchHistroies)
+      this.searchHistories.unshift(searchText)
 
       // 展示搜索结果
       this.isResultShow = true
@@ -86,17 +90,19 @@ export default {
     async loadSearchHiestories () {
       // 这里需要让后端返回的历史记录和本地的历史记录合并到一起(因为后端只储存了四条)
       // 如果用户已登录
-      let searchHistories = getItem('search-histories') || []
-      if (this.user) {
-        const { data } = await getSearchHistories()
-        // 合并数组： [...数组, ...数组]
-        // 把Set转为数组：[...Set对象]
-        // 数组去重：[...new Set([...数组, ...数组])
-        searchHistories = [...new Set([
-          ...searchHistories,
-          ...data.data.keywords
-        ])]
-      }
+      // let searchHistories = getItem('search-histories') || []
+      // if (this.user) {
+      //   const { data } = await getSearchHistories()
+      //   // 合并数组： [...数组, ...数组]
+      //   // 把Set转为数组：[...Set对象]
+      //   // 数组去重：[...new Set([...数组, ...数组])
+      //   searchHistories = [...new Set([
+      //     ...searchHistories,
+      //     ...data.data.keywords
+      //   ])]
+      // }
+
+      const searchHistories = getItem('search-histories') || []
 
       this.searchHistories = searchHistories
     }
